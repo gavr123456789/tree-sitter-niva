57674c45d2545cdec9526ae646b4b9b01b04d10b
gavr123456789 <gavr123456789@gmail.com>
2026-02-28 20:20:47 +0200

    fix errors "missing required capture(s) in niva indents TreeSitter query: indent" missing required capture(s) in niva outline TreeSitter query: item, name

--- queries/indents.scm
+++ queries/indents.scm
@@ -1,22 +1,4 @@
-; [
-;   (block)
-;   (arguments)
-;   (parameter)
-;   (class_definition)
-;   (trait_definition)
-;   (object_definition)
-;   (function_definition)
-;   (val_definition)
-;   (import_declaration)
-;   (while_expression)
-;   (do_while_expression)
-;   (for_expression)
-;   (try_expression)
-;   (match_expression)
-; ] @indent
+; Indentation rules for Zed.
+; Keep it minimal: any block increases indentation.
 
-(_ "[" "]" @end) @indent
-(_ "{" "}" @end) @indent
-(_ "#{" "}" @end) @indent
-(_ "(" ")" @end) @indent
-(_ "#(" ")" @end) @indent
+(block) @indent
