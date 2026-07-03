c4c4ac1db640055bde17cf8f9e33f5bf1d616493
gavr123456789 <gavr123456789@gmail.com>
2026-07-03 23:51:35 +0300

    I dont understand this shit

--- queries/indents.scm
+++
@@ -1,7 +0,0 @@
-; Indentation rules for Zed.
-; @indent  - mark a node whose inner lines should be indented (usually a block).
-; @end     - mark the token that closes the block; its line is outdented back.
-
-(block "]" @end) @indent
-(block "}" @end) @indent
-(block ")" @end) @indent
